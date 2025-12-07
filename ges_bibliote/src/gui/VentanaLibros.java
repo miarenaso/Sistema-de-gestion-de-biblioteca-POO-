f3a2a2898f80844399a6030f4f70e95a2617c094
memita010203 <mmirandaa@unal.edu.co>
2025-12-06 19:43:42 -0500

    Delete ges_bibliote/src/gui/VentanaLibros.java

--- ges_bibliote/src/gui/VentanaLibros.java
+++
@@ -1,5 +0,0 @@
-package gui;
-//La idea es que en gui subamos todo lo referente a la interfaz gráfica. Por el path que tiene es más sencillo comunicarlo con App. java. Según entineod debemos hacer alguna clase dentro de modelo en donde haremos la funcionalidad como una clase para que app sólo se comunique con front y back :D
-public class VentanaLibros {
-
-}
